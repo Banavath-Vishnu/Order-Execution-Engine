0914597f110a41fcbeaa71451cdb0ee01e37b735
Banavath Vishnu <142583592+Banavath-Vishnu@users.noreply.github.com>
2025-11-22 20:57:23 +0530

    Update queue.ts

--- src/queue.ts
+++ src/queue.ts
@@ -1,13 +1,17 @@
+// src/queue.ts
 import { Queue } from 'bullmq';
 import IORedis from 'ioredis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connection = new IORedis(process.env.REDIS_URL, { 
+// FIX: Cast "as string" to shut up the compiler
+const redisUrl = (process.env.REDIS_URL as string) || 'redis://127.0.0.1:6379';
+
+const connection = new IORedis(redisUrl, { 
   maxRetriesPerRequest: null 
 });
 
-const prefix = process.env.QUEUE_PREFIX || 'order-engine';
+const prefix = (process.env.QUEUE_PREFIX as string) || 'order-engine';
 
 export const queue = new Queue('orders', {
   connection,
