18321b9bb72c19bc733e23e852447dc3c4545436
Banavath Vishnu <142583592+Banavath-Vishnu@users.noreply.github.com>
2025-11-22 20:46:33 +0530

    Refactor Redis connection to use QUEUE_PREFIX

--- src/queue.ts
+++ src/queue.ts
@@ -3,7 +3,7 @@ import IORedis from 'ioredis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connection = new IORedis(process.env.REDIS_URL || 'redis://127.0.0.1:6379', { 
+const connection = new IORedis(process.env.REDIS_URL, { 
   maxRetriesPerRequest: null 
 });
 
